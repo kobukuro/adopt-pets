6540525c01aad971036f219b1804e642c6f1795b
Peter Chen <peter.kt.chen@mail.foxconn.com>
2021-02-04 09:16:03 +0800

    fixed comment bug

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-//store為global object，存放global variable以及function等，讓所有components使用
+// store為global object，存放global variable以及function等，讓所有components使用
 import Vue from 'vue'
 import Vuex from 'vuex'
 
